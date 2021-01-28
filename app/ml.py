f579a363110df5f7e7b4c8dbb6bf18abb9ee2311
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-01-27 21:51:51 -0500

    population.py is copy of population jupyter notebook

--- app/ml.py
+++ app/ml.py
@@ -3,3 +3,8 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.get('/hello_user_ml')
+async def hello_user(name = 'Ike'):
+    '''Return simple gretting'''
+    return {'username' : f'Hello {name}'}
